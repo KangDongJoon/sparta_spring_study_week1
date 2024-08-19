1edaf5234fc644114cc48090a61874e604a1eef7
KangDongJoon <ehdwns1119@naver.com>
2024-08-19 11:44:27 +0900

    [FEAT] 메모 조회하기 api 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,15 +3,9 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +14,7 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping("/memos")
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // ReqyestDto -> Entity
         Memo memo = new Memo(requestDto);
 
@@ -35,5 +29,14 @@ public class MemoController {
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
 
         return memoResponseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos(){
+        // Map to List
+        List<MemoResponseDto> reponseList = memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+
+        return reponseList;
     }
 }
